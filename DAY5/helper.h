246be6c0ef200904f3b5d06cae45a47e07bb5851
ecourse <smkang@codenuri.co.kr>
2023-02-17 12:53:40 +0900

    Add files via upload

--- DAY5/helper.h
+++ DAY5/helper.h
@@ -5,5 +5,17 @@
 // 2. 자주 사용하는 매크로
 // 3. 구조체 정의 등을 넣게 됩니다. - 오늘 오후!
 
+// 188 page
+// include guard : 
+//#ifndef HELPER_H
+//#define HELPER_H
+
+#pragma once  // 2번 include 되어도 한번만 컴파일 해달라는
+				// 최신 매크로.
+
 #define WIDTH	10
 #define HEIGHT	10
+
+int add(int a, int b) { return a + b; }
+
+//#endif
