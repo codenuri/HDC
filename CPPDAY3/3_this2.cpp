22e32996fee67eecda56cebe477858a02f67c298
ecourse <smkang@codenuri.co.kr>
2023-02-23 08:47:54 +0900

    Add files via upload

---
+++ CPPDAY3/3_this2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	void set(int x, int y) 
+	{
+		// 멤버 데이타 이름과 인자의 이름이 동일하면
+		// => 에러는 아니고 x 사용시 인자를 의미 합니다.
+		x = x;	 // 인자 x = 인자 x
+
+		this->x = x; // 멤버 x = 인자 x
+		this->y = y;
+
+		// this 활용 1.
+		// => 이름 충돌시 멤버임을 명확히 표기하기 위해
+	}
+	// 활용 2. this 를 반환하면 멤버 함수를 연속적으로 
+	//	      호출 가능합니다.
+	Point* foo() { return this; }
+	//Point  goo() { return *this; }
+	Point&  goo() { return *this; }
+};
+int main()
+{
+	Point p1;
+	p1.set(10, 20);
+
+//	p1.foo()->foo()->foo()->foo();
+//	p1.goo().goo().goo().goo();
+
+//	std::cout << "A" << "B" << "C" << "D";
+	//cout.operator<<("A").operator<<("B").operator<<("C")
+}
---
+++ CPPDAY3/4_상수멤버함수2.cpp
@@ -0,0 +1,40 @@
+﻿
+// github.com/webkit	=> 사파리브라우져 소스
+//						=> 크롬 브라우져의 초기 버전
+// webkit/source/wtf/wtf/seconds.h 열어 보세요
+
+// 상수 멤버 함수는 "선택"이 아닌 "필수" 문법 입니다.
+
+// 객체의 상태를 변경하지 않은 모든 멤버 함수는(getxxx)
+// "반드시" 상수 멤버 함수로 해야 합니다.
+
+class Rect
+{
+	int x, y, w, h;
+public:
+	Rect(int a, int b, int c, int d)
+		: x(a), y(b), w(c), h(d) {}
+
+//	int getArea() { return w * h; } // 잘못 만든 코드
+	int getArea() const { return w * h; } // ok
+};
+
+
+// void foo(Rect r) // bad. 사용자 정의 타입은
+					//      call by value 사용하지 마세요
+void foo(const Rect& r) // good. 사용자 정의 타입은 const &로 받으세요
+{
+	int n = r.getArea(); // ?
+}
+
+int main()
+{
+	Rect r(1, 1, 10, 10);	// 상수 객체 아님.
+
+	int n = r.getArea();	// ok
+	
+	foo(r);
+}
+
+
+
